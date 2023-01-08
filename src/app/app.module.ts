75851a4759df3bb6e35289ad84c31fad9db2ee1f
LuczaG <gergo.lucza@gmail.com>
2023-01-08 17:23:33 +0100

    Created Car-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 
 import { AppComponent } from './app.component';
 import { CarsComponent } from './cars/cars.component';
+import { CarDetailComponent } from './car-detail/car-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent
+    CarsComponent,
+    CarDetailComponent
   ],
   imports: [
     BrowserModule,
